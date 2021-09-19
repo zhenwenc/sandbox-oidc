2304fbc6b9c999e62b40994f7a944575f878409c
Ivy Yang <ivy.yang@mattr.global>
2021-09-19 20:29:14 +1200

    fix: parse Azure AD callback url

--- src/pages/callback.tsx
+++ src/pages/callback.tsx
@@ -1,19 +1,22 @@
-import { parse } from 'query-string';
-import { isEmpty } from 'ramda';
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import { Message } from '@navch-ui/core';
+import { parse } from "query-string";
+import { isEmpty } from "ramda";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { Message } from "@navch-ui/core";
 
-import { Layout } from '@components/Layout';
-import { InteractionResult } from '@components/InteractionResult';
+import { Layout } from "@components/Layout";
+import { InteractionResult } from "@components/InteractionResult";
 
 export default function CallbackScreen() {
   const router = useRouter();
-  const query = router.asPath.includes('#') ? parse(router.asPath.split('#', 2)[1]) : router.query;
+  const url = router.asPath;
+  const query = (url.includes("?") || !url.includes("#"))
+    ? router.query
+    : parse(url.split("#", 2)[1]);
 
   const [params, setParams] = useState<Record<string, string>>();
   useEffect(() => {
-    console.debug('[CallbackScreen] Parsed params:', query);
+    console.debug("[CallbackScreen] Parsed params:", query);
 
     if (isEmpty(query)) return;
     setParams(query as Record<string, string>);
